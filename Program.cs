f6811ca5678a18120199d3102419b944e28f42a9
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-13 16:42:28 -0500

    save before heading home

--- Program.cs
+++ Program.cs
@@ -4,19 +4,21 @@ namespace vehicles
 {
     public interface IVehicle
     {
-        int Wheels { get; set; }
-        int Doors { get; set; }
+        string Propulsion { get; set; } 
+        int Doors { get; set; } 
         int PassengerCapacity { get; set; }
-        bool Winged { get; set; }
-        string TransmissionType { get; set; }
-        double EngineVolume { get; set; }
-        double MaxWaterSpeed { get; set; }
-        double MaxLandSpeed { get; set; }
-        double MaxAirSpeed { get; set; }
-        void Start();
-        void Stop();
-        void Drive();
-        void Fly();
+        bool Winged { get; set; } //air vehicle
+        // string TransmissionType { get; set; }
+        // double EngineVolume { get; set; }
+        double MaxWaterSpeed { get; set; } //water vehicle
+        double MaxLandSpeed { get; set; } //land vehicle
+        double MaxAirSpeed { get; set; } // air vehicle
+        // void Start();
+        // void Stop();
+        // void Drive(); 
+        // void Fly();
+        void vehicleOperation();
+
     }
     class Program
     {
