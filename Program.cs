93be7022a1aaa988e5868b02e80657226e9c17f8
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-13 15:57:57 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace vehicles
+{
+    public interface IVehicle
+    {
+        int Wheels { get; set; }
+        int Doors { get; set; }
+        int PassengerCapacity { get; set; }
+        bool Winged { get; set; }
+        string TransmissionType { get; set; }
+        double EngineVolume { get; set; }
+        double MaxWaterSpeed { get; set; }
+        double MaxLandSpeed { get; set; }
+        double MaxAirSpeed { get; set; }
+        void Start();
+        void Stop();
+        void Drive();
+        void Fly();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
